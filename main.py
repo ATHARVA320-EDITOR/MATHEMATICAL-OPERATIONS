474ee65e630145d64a39ed6739e347c3780c2d0a
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-11-21 16:42:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+print(math.sqrt(4))
+print(math.pow(3,2))
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+print(math.ceil(45.490))
+print(math.floor(45.490))
+print(math.factorial(5))
